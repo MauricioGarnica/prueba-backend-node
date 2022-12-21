b70d9b37e05219bc7417ac32f358801497b69e8d
Mauricio Garnica <edgarmauriciog99@gmail.com>
2022-12-21 11:09:24 -0600

    Se cambio el puerto

--- database/connection.ts
+++ database/connection.ts
@@ -4,7 +4,8 @@ const conn = mysql.createConnection({
     host: process.env.HOST || 'localhost',
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || 'root',
-    database: process.env.DATABASE || 'prueba_backend'
+    database: process.env.DATABASE || 'prueba_backend',
+    port: 3306 || process.env.DB_PORT
 });
 
 export default conn;
--- dist/database/connection.js
+++ dist/database/connection.js
@@ -8,7 +8,8 @@ const conn = mysql_1.default.createConnection({
     host: process.env.HOST || 'localhost',
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || 'root',
-    database: process.env.DATABASE || 'prueba_backend'
+    database: process.env.DATABASE || 'prueba_backend',
+    port: 3306 || process.env.DB_PORT
 });
 exports.default = conn;
 //# sourceMappingURL=connection.js.map
